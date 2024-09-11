0a6dadec70d14d49e2644fc99ea6ddbc9d682b2b
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2024-09-11 12:02:52 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
 const button4 = document.querySelector("#button4"); 
-const button5 = document.querySelector("#button5");
+
 const healthText = document.querySelector("#healthText");
 const atkText = document.querySelector("#atkText");
 const text = document.querySelector("#text"); 
@@ -230,7 +230,7 @@ button1.onclick = goLargeCavern;
 button2.onclick = goDeathRunAway;
 button3.onclick = goDeathRunAway;
 button4.onclick = goDeathRunAway;
-button5.onclick = goDeathRunAway;
+
 
 function update(locations) {
   button1.onclick = locations["button functions"][0];
