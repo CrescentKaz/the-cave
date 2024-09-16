50ca909b5c0480ff86fb438889203ed78ec9df68
CrescentKaz <121899324+CrescentKaz@users.noreply.github.com>
2024-09-15 21:24:28 -0500

    Update script.js

--- script.js
+++ script.js
@@ -59,126 +59,126 @@ const locations = [
     room: "Entrance",
     "button functions": [goLargeCavern, goDeathRunAway, goDeathRunAway, goDeathRunAway],
     text: "Before you looms the dark, decrepit crags of Mt Feir, so named for the fear it instills upon all who tred it\'s trecherous terrain. You are a knight tasked by the King and Queen to slay the dragon inhabiting Mt Feir and freeing the people of it\'s villany. The large gaping maw of the mountain lies to the North of you.",
-    picture: "./images/the-cave.png"
+    picture: "./images/The_Cave.PNG"
   },
   {
     name: "largeCavern",
     room: "Large Cavern",
     "button functions": [goUndergroundRiver, goDeadEnd, goTunnelOfBio, goEntrance],
     text: "You enter a large cavern littered with stalagtites, stalagmites, and columns of various widths. It is dark and dank. The faint scent of muggy decay wafts gently accross the ground. Soft dripping echoes around you as you catch flashes of sunlight reflecting from the falling droplettes.",
-    picture: "./images/large-cavern.png"
+    picture: "./images/Large_Cavern.PNG"
   },
   {
     name: "hiddenRoom",
     room: "Hidden Room",
     "button functions": [goDeadEnd, goDeadEnd, goLargeCavern, goDeadEnd],
     text: "You fly up and spot a small alcove to the east. As your feet touch the ground in the alcove you see a tiny engraving on the wall, \"Created by Kaitlyn Johnson.\" ",
-    picture: "./images/hidden-room.png"
+    picture: "./images/Hidden_Room.PNG"
   },
   {
     name: "tunnelOfBio",
     room: "Tunnel of Bioluminescence",
     "button functions": [goDeadEnd, goLargeCavern, goDeadEnd, goDeadEnd],
     text: "The tunnel literally glows as biolumenencent algae and cave crawlers line the walls and ceiling. They\'re mostly teal with some flat pinks and neon yellows. To the East is the large carvern. Above is a dim golden light. If only there was a way to get up there...",
-    picture: "./images/tunnel-of-bio.png"
+    picture: "./images/Tunnel_of_Bio.PNG"
   },
   {
     name: "undergroundRiver",
     room: "Underground Stream",
     "button functions": [goOldTracks, goBatCave, goOldCampsite, goLargeCavern], 
     text: "In front of you is a rushing stream. Glowing algae lights the room. There\'s a path to the west and to the east, the water is low enough to follow to the north.",
-    picture: "./images/underground-river.png"
+    picture: "./images/Underground_River.PNG"
   },
   {
     name: "oldCampsite",
     room: "Old Campsite",
     "button functions": [goDeadEnd, goUndergroundRiver, goDeadEnd, goDeadEnd],
     text: "You find an old campsite with a burnt out firepit, a tattered dusty tent that leans too far to the right, and a few large rocks that might have doubled as stools. Luckily, you don\'t find any bodies. You do find a small empty knapsack and a book tucked just inside the tent. To the East lies the underground stream.",
-    picture: "./images/old-campsite.png"
+    picture: "./images/Old_Campsite.PNG"
   },
   {
     name: "batCave",
     room: "Bat Cave",
     "button functions": [goDeadEnd, goDeadEnd, goUndergroundRiver, goDeadEnd],
     text: "You enter a spacous warm cave with lots of high pitched chittering. You look up to see the ceiling absolutely covered in small black bodies. The ground underfoot is a bit mushy. You found a bat cave! To the West awaits the underground stream.",
-    picture: "./images/bat-cave.png"
+    picture: "./images/Bat_Cave.PNG"
   },
   {
     name: "oldTracks",
     room: "Coin Tracks",
     "button functions": [goSortaOldTracks, goDeadEnd, goDeadEnd, goUndergroundRiver],
     text: "A small coin catches the light and brings your attention to large depressions that trail off to the North. You can\'t tell for certain if they\'re  going towards the north or from the north, but there\'s only one way to find out. The underground stream is to the South if you\'re too scared.",
-    picture: "./images/coin-tracks.png"
+    picture: "./images/Tracks_Coin.PNG"
   },
   {
     name: "sortaOldTracks",
     room: "Sapphire Tracks",
     "button functions": [goOldCarving, goWeekOldTracks, goBoneRoom, goOldTracks],
     text: "The world gets still and quiet as you track your target. Sweat beads apon your brow and your hair sticks to the sides of your face. Are you ready for this? Did you get enough training? Did you eat enough for breakfast? What if you ate too much? You think the dragon would leave the village alone if you threw up on it? Better not test that... You\'re at a crossroads in the mountain. You can vaugely make out some sort of carving to the North, the coin tracks lie to the South, a pink glint rests in the tracks that head East and a rancid scent wafts towards you from the West.",
-    picture: "./images/sapphire-tracks.png"
+    picture: "./images/Tracks_Sapphire.PNG"
   },
   {
     name: "boneRoom",
     room: "Bone Room",
     "button functions": [goDeadEnd, goSortaOldTracks, goDeadEnd, goDeadEnd],
     text: "Bones of full 50 men lie strewn about. Some are broken, others whole, and yet others with many many needle-thin stalagtites poking straight through. You think it might be best to go back the way you came.",
-    picture: "./images/bone-room.png"
+    picture: "./images/Bone_Room.PNG"
   },
   {
     name: "oldCarving",
     room: "Old Carving", 
     "button functions": [goDeadEnd, goDeadEnd, goDeadEnd, goSortaOldTracks],
     text: "Before you are crude letters carved into the wall of rock. Written in Aramaic, it reads \"If you do doubt your courage or your strength come no further. Only he who is valiant and pure of spirit may enter the Cave of Aaaaaargh\" ... they must have died while carving it.",
-    picture: "./images/old-carving.png"
+    picture: "./images/Old_Carving.PNG"
   },
   {
     name: "weekOldTracks",
     room: "Ruby Tracks",
     "button functions": [goDarkRoom, goDeadEnd, goSortaOldTracks, goNewerTracks],
     text: "The air is stale and heavy as you make your way around a curve in the trail. A bead of sweat trickles down your calf and you peer into the dim light. The tracks deffinately went this way... did the King and Queen know they dragon could shapeshift...? To the North is a hole in the wall, too dark to see into. The tracks appear to head around to the South. You hope this hasn\'t been a large loop.",
-    picture: "./images/ruby-tracks.png"
+    picture: "./images/Tracks_Ruby.PNG"
   },
   {
     name: "darkRoom",
     room: "Dark Room",
     "button functions": [goDeadEnd, goDeadEnd, goMiniGame, goWeekOldTracks],
     text: "It is too dark in here to see. To the South you see the hole you just crawled through.",
-    picture: "./images/dark-room.png"
+    picture: ""
   },
   {
     name: "newerTracks",
     room: "Golden Tracks",
     "button functions": [goWeekOldTracks, goCavePainting, goFreshTracks, goDeadEnd],
     text: "The air is still a little stale but you catch a whiff of a musky scent. You gently brush your left foot across the edge of one track print ringed in gold. The tracks point off to the West. You just came from the North.", 
-    picture: "./images/golden-tracks.png"
+    picture: "./images/Tracks_Gold.PNG"
   },
   {
     name: "cavePainting",
     room: "Cave Painting",
     "button functions": [goDeadEnd, goDeadEnd, goNewerTracks, goDeadEnd],
     text: "You see rust colored smears criss-crossing the wall. They spell out, \"KRONOS\"... some sort of password maybe?",
-    picture: "./images/cave-painting.png"
+    picture: "./images/Cave_Painting.PNG"
   },
   {
     name: "freshTracks",
     room: "Fresh Tracks",
     "button functions": [goDeadEnd, goNewerTracks, goTreasureHoard, goDeadEnd],
     text: "You crouch down slowly, mindful of the noise from your armor. Your fingers brush against the easily disturbed ground. These tracks are very fresh, possibly from today, and the pungent odor of lizard musk confirms it. You came from the East. To the west you see the faint glow of what appears to be a rainbow all out of order.",
-    picture: "./images/fresh-tracks.png"
+    picture: "./images/Fresh_Tracks.PNG"
   },
   {
     name: "treasureHoard",
     room: "Treasure Hoard",
     "button functions": [goDragonRoom, goFreshTracks, goDeadEnd, goDeadEnd],
     text: "Red, green, blue, purple, yellow, and orange lights raidiate from all kinds of gems. Gold coins, ingots, and bars overflow from giant chalises and chests. Strings of pearls hang from stalagtites. Shimmering silks swath ornate silver-wraught forms. Sapphires litter the floor like a river and a small stream of rubies and garnets seem to pour out of a pile of emeralds like lava over a lush mountainside. The entire room is a walk-through-able art piece with a path going from East to North.",
-    picture: "./images/treasure-hoard.png"
+    picture: "./images/Treasure_Hoard.PNG"
   },
   {
     name: "dragonRoom",
     room: "Dragon Room",
     "button functions": [goDeadEnd, goDeathBoneRoom, goTunnelOfBio, goTreasureHoard],
     text: "You blink as your eyes adjust to the dimly lit room. A loud snoring makes you jump and you notice a large dark colored dragon asleep right next to you. What do you do?",
-    picture: "./images/dragon-room.png"
+    picture: "./images/Dragon.PNG"
   },
 ]; 
 
@@ -189,7 +189,7 @@ const miscLocals = [
     "button text": ["N", "E", "W", "S"],
     "button functions": [restart, restart, restart, restart],
     text: "You manage to slay the dragon! YOU WIN! 🎉 Ending 1 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: "./images/Win_Sword.PNG"
   },
   {
     name: "winFire",
@@ -197,7 +197,7 @@ const miscLocals = [
     "button text": ["N", "E", "W", "S"],
     "button functions": [restart, restart, restart, restart],
     text: "You incinerate the shapeshifting dragon and almost kill yourself in the process by burning away all the oxygen in the cave! Seriously, might want to rethink that next time... YOU WIN! 🎉 Ending 2 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: "./images/Win_Fire.PNG"
   },
   {
     name: "miniGame",
@@ -205,7 +205,7 @@ const miscLocals = [
     "button text": ["1", "0", "3", "2"],
     "button functions": [pickOne, goDarkRoom, pickThree, pickTwo],
     text: "You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win (but don\'t escape the dragon)! (Press 0 to go back.)",
-    picture: "./images/.png"
+    picture: ""
   },
   {
     name: "fightFrog",
@@ -213,7 +213,7 @@ const miscLocals = [
     "button text": ["A", "B", "C", " "],
     "button functions": [attack, dodge, goDeathRunAway, doNothing],
     text: "A frog has picked a fight with you! What do you do? (A - attack, B - dodge, C - flee)",
-    picture: "./images/frog.png"
+    picture: "./images/Frog.PNG"
   },
   {
     name: "fightWorm",
@@ -221,7 +221,7 @@ const miscLocals = [
     "button text": ["A", "B", "C", " "],
     "button functions": [attack, dodge, goDeathRunAway, doNothing],
     text: "A worm has picked a fight with you! What do you do? (A - attack, B - dodge, C - flee, D - ?)",
-    picture: "./images/worm.png"
+    picture: "./images/Worm.PNG"
   },
   {
     name: "fightBat",
@@ -229,7 +229,7 @@ const miscLocals = [
     "button text": ["A", "B", "C", " "],
     "button functions": [attack, dodge, goDeathRunAway, doNothing],
     text: "A bat has picked a fight with you! What do you do? (A - attack, B - dodge, C - flee, D - ?)",
-    picture: "./images/.png"
+    picture: "./images/Bat.PNG"
   },
   {
     name: "fightDragon",
@@ -237,7 +237,7 @@ const miscLocals = [
     "button text": ["A", "B", "C", " "],
     "button functions": [attack, dodge, goDeathRunAway, doNothing],
     text: "The dragon has picked a fight with you! What do you do? (A - attack, B - dodge, C - flee, D - ?)",
-    picture: "./images/.png"
+    picture: "./images/Dragon.PNG"
   },
 ];
   
@@ -245,37 +245,37 @@ const deaths = [
   { 
     name: "boneRoom",
     text: "You slip and slide down a hole onto bones of full 50 men strewn about. Some are broken, others whole, and yet others with many many thin stalagmites piercing straight through. Unfortuneately you land on one of these. It is very painful... You Died. ☠️ Ending 3 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: ""
   },
   {
     name: "runAway",
     text: "You run away from your duties. You are branded a traitor and banished to the shadowlands! Ending 4 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: ""
   },
   {
     name: "takeTreasure",
     text: "ENTER TEXT HERE Ending 5 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: ""
   },
   {
     name: "monsterDidIt",
     text: "You died to a cave monster. How shameful... Ending 6 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: ""
   },
   {
     name: "dragonStick",
     text: "You attempted to beat the dragon with a stick, while he slept. It did not work out for you. Maybe if you had a horse and a lance... Ending 7 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: ""
   },
   {
     name: "dragonFist",
     text: "You punched the dragon in his face then bit your lip in pain as your bare knuckles began to ooze blood from the fresh wound. Dragon\'s have ganoid scales on their face so you basically punched a rock covered in sandpaper. The dragon's bose twitches and his eyes fly open, pupils diolated wide. He takes one second to stare at you before eating you whole. You died. Ending 8 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: ""
   },
   {
     name: "dragonLost",
     text: "After a long a tiresome struggle, you try and try but can\'t seem to hit the thing! The dragon shifts its shape as easily as water flows downhill, causing you to miss repeatedly. You manage to strike it with one solid good thrust only to find your sword now logged between two scales. You kick the hilt, causing some twisted damage. The dragon howls in pain and anger, then begins it\'s counterattack. It lunges at you, teeth barely missing your shoulder. He steps forward and lunges again. This time you roll backwards to avoid it. You don\'t know if your armor can take another blow and don\'t want to risk finding out. The dragon shifts into a large serpent and strikes you fast and true on your exposed cheek. Pain races through your nerves and your eyes lock up at the ceiling. You can\'t move. A roaring waterfall in your ears begins to drown out everything else. Your body crumples to the ground but you can\'t feel it. Can\'t hear it. Soon, you can\'t see the cave ceiling anymore... Ending 9 of 10 (press any direction to continue)",
-    picture: "./images/.png"
+    picture: ""
   }
 ];
 
